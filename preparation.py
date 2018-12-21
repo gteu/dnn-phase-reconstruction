18843f013975e806464b0d9c0d5d7eab4234b697
gteu <syunsignui@aol.jp>
2018-12-21 15:07:05 +0900

    update files

--- preparation.py
+++ preparation.py
@@ -6,44 +6,106 @@ import cmath
 import glob
 import os
 import random
+np.set_printoptions(threshold=np.inf)
 
-prepare_feats = True
+from src import utils
+
+prepare_feats = False
 norm_feats = True
 
 data_shuffle = True
 
-inp_dim = 513
+ab_dim = 1285
+
+inp_stats_file = "data/input_MVN_1285.norm"
 
 # from .wav to .ab(magnitude of frequency) and .ph(phase of frequency)
 if prepare_feats:
     wavs = glob.glob("data/drums/*/*.wav")
+    num_of_wavs = len(wavs)
 
     if data_shuffle:
         random.shuffle(wavs)
 
-    for i in range(len(wavs)):
+    # create file_id_list.scp
+    fid = open("file_id_list.scp", mode="w")
+
+    # for i in range(num_of_wavs):
+    for i in range(2000):
+        # wav_name = os.path.splitext(os.path.basename(wavs[i]))[0]
+        # fid.write(wav_name)
+        fid.write(str(i))
+        if not i == num_of_wavs - 1:
+            fid.write("\n")
+
+    fid.close()
+
+    # create .ab and .ph
+    # for i in range(num_of_wavs):
+    for i in range(2000):
         wav = wavs[i]
-        print(wav)
-        y, sr = librosa.load(wav, sr=22050)
-        D = librosa.stft(y, n_fft=1024, hop_length=128)
+        # wav_name = os.path.splitext(os.path.basename(wavs[i]))[0]
+        print(i, wav)
+        y, sr = librosa.load(wav, sr=16000)
+        D = librosa.stft(y, n_fft=512, hop_length=80, win_length=400)
         ab = np.abs(D)
-        phase = np.angle(D)
+        ph = np.angle(D)
+
+        joint_ab = np.empty((ab.shape[0] * 5, ab.shape[1]), dtype=np.float32)
+        joint_ph = np.empty((ph.shape[0] * 5, ph.shape[1]), dtype=np.float32)
+
+        for j in range(ab.shape[1]):
+            temp_ab = np.zeros((ab.shape[0], 5), dtype=np.float32)
+            temp_ph = np.zeros((ph.shape[0], 5), dtype=np.float32)
+            for k in range(-2, 3):
+                if j + k < 0 or j + k > ab.shape[1] - 1:
+                    pass
+                else:
+                    temp_ab[:,k+2] = ab[:,j+k]
+                    temp_ph[:,k+2] = ph[:,j+k]
+
+            joint_ab[:,j] = np.concatenate([temp_ab[:,0],temp_ab[:,1],temp_ab[:,2],temp_ab[:,3],temp_ab[:,4]], axis=0)
+            joint_ph[:,j] = np.concatenate([temp_ph[:,0],temp_ph[:,1],temp_ph[:,2],temp_ph[:,3],temp_ph[:,4]], axis=0)
+
+        # ab_path = os.path.join("data/abs", wav_name + ".ab")
+        # ph_path = os.path.join("data/phase", wav_name + ".ph")
         ab_path = os.path.join("data/abs", str(i) + ".ab")
-        phase_path = os.path.join("data/phase", str(i) + ".ph")
-        ab.tofile(ab_path)
-        phase.tofile(phase_path)
+        ph_path = os.path.join("data/phase", str(i) + ".ph")
+        joint_ab.tofile(ab_path)
+        joint_ph.tofile(ph_path)
 
 
-# normalize input data to have zero-mean unit-variance
+# normalize input data(.ab) to have zero-mean unit-variance
 if norm_feats:
     ab_files = glob.glob("data/abs/*.ab")
     num_of_ab = len(ab_files)
 
+    ab_dir = "data/abs"
     norm_ab_dir = "data/norm_abs"
     if not os.path.exists(norm_ab_dir):
         os.makedirs(norm_ab_dir)
 
-    # for f in range(num_of_ab):
-    #     file_x = np.fromfile(inp_file_list[i], dtype=np.float32)
-    #     file_x = np.reshape(file_x, (inp_dim, -1))
-    #     file_x = file_x.T
+    ab_file_id_list = []
+    fid = open("file_id_list.scp", mode="r")
+    for line in fid.readlines():
+        line = line.strip()
+        if len(line) < 1:
+            continue
+        ab_file_id_list.append(line)
+    fid.close()
+
+    ab_file_list = utils.prepare_file_path_list(ab_file_id_list, ab_dir, ".ab")
+    all_ab = utils.read_data_from_one_list(ab_file_list, ab_dim)
+
+    inp_scaler = utils.compute_norm_stats(all_ab, inp_stats_file)
+
+    for i in range(num_of_ab):
+        ab = np.fromfile(ab_file_list[i], dtype=np.float32)
+        ab = np.reshape(ab, (ab_dim, -1))
+        ab = ab.T
+
+        norm_ab = inp_scaler.transform(ab)
+        norm_ab_path = os.path.join(norm_ab_dir, os.path.basename(ab_file_list[i]))
+        norm_ab = norm_ab.T
+
+        norm_ab.tofile(norm_ab_path)
--- train.py
+++ train.py
@@ -8,58 +8,59 @@ from keras.layers import Dense
 from keras.layers import Dropout
 from keras.optimizers import SGD, Adam, Adagrad
 
-def read_data_from_list(inp_file_list, out_file_list, inp_dim, out_dim):
-    temp_x = np.empty((FRAME_BUFFER_SIZE, inp_dim))
-    temp_y = np.empty((FRAME_BUFFER_SIZE, out_dim))
-
-    current_index = 0
-
-    assert len(inp_file_list) == len(out_file_list)
-    num_of_sounds = len(inp_file_list)
-
-    for i in range(num_of_sounds):
-        file_x = np.fromfile(inp_file_list[i], dtype=np.float32)
-        file_x = np.reshape(file_x, (inp_dim, -1))
-        file_x = file_x.T
-
-        file_y = np.fromfile(out_file_list[i], dtype=np.float32)
-        file_y = np.reshape(file_y, (out_dim, -1))
-        file_y = file_y.T
-
-        assert file_x.shape[0] == file_y.shape[0]
-        frame_number = file_x.shape[0]
-
-        temp_x[current_index:current_index+frame_number] = file_x[0:frame_number]
-        temp_y[current_index:current_index+frame_number] = file_y[0:frame_number]
-
-        current_index += frame_number
-
-    temp_x = temp_x[0:current_index]
-    temp_y = temp_y[0:current_index]
-
-    return temp_x, temp_y
-
-def prepare_file_path_list(file_id_list, file_dir, file_extension):
-    file_path_list = []
-    for file_id in file_id_list:
-        file_path = file_dir + "/" + file_id + file_extension
-        file_path_list.append(file_path)
-
-    return file_path_list
+from src import utils
 
 def phase_loss(y_true, y_pred):
     return
 
-FRAME_BUFFER_SIZE = 3000000
+# train_file_number = 6000
+# valid_file_number = 700
+# test_file_number = 49
 
-train_file_number = 6000
-valid_file_number = 700
-test_file_number = 49
+train_file_number = 1850
+valid_file_number = 100
+test_file_number = 50
 
-inp_dim = 513
-out_dim = 513
+inp_dim = 1285
+out_dim = 257
 
-inp_feat_dir = "data/abs"
+# DNN configuration
+hidden_layer_type = ['relu', 'relu', 'relu']
+hidden_layer_size = [1024, 1024, 1024]
+
+output_type = 'linear'
+dropout = 0.0
+loss_function = phase_loss
+optimizer = Adam()
+shuffle_data = True
+
+num_of_epochs = 100
+
+model = Sequential()
+
+for i in range(len(hidden_layer_type)):
+    if i == 0:
+        input_size = inp_dim
+    else:
+        input_size = hidden_layer_size[i - 1]
+
+    model.add(Dense(
+            units=hidden_layer_size[i],
+            activation=hidden_layer_type[i],
+            kernel_initializer="normal",
+            input_dim=input_size))
+
+    model.add(Dropout(dropout_rate))
+
+final_layer = model.add(Dense(
+    units=out_dim,
+    activation=output_type.lower(),
+    kernel_initializer="normal",
+    input_dim=hidden_layer_size[-1]))
+
+model.compile(loss=loss_function, optimizer=optimizer)
+
+inp_feat_dir = "data/norm_abs"
 out_feat_dir = "data/phase"
 
 inp_file_ext = ".ab"
@@ -83,14 +84,14 @@ train_id_list = file_id_list[0: train_file_number]
 valid_id_list = file_id_list[train_file_number: train_file_number + valid_file_number]
 test_id_list  = file_id_list[train_file_number + valid_file_number: train_file_number + valid_file_number + test_file_number]
 
-inp_train_file_list = prepare_file_path_list(train_id_list, inp_feat_dir, inp_file_ext)
-out_train_file_list = prepare_file_path_list(train_id_list, out_feat_dir, out_file_ext)
+inp_train_file_list = utils.prepare_file_path_list(train_id_list, inp_feat_dir, inp_file_ext)
+out_train_file_list = utils.prepare_file_path_list(train_id_list, out_feat_dir, out_file_ext)
 
-inp_valid_file_list = prepare_file_path_list(valid_id_list, inp_feat_dir, inp_file_ext)
-out_valid_file_list = prepare_file_path_list(valid_id_list, out_feat_dir, out_file_ext)
+inp_valid_file_list = utils.prepare_file_path_list(valid_id_list, inp_feat_dir, inp_file_ext)
+out_valid_file_list = utils.prepare_file_path_list(valid_id_list, out_feat_dir, out_file_ext)
 
-inp_test_file_list = prepare_file_path_list(test_id_list, inp_feat_dir, inp_file_ext)
-out_test_file_list = prepare_file_path_list(test_id_list, out_feat_dir, out_file_ext)
+inp_test_file_list = utils.prepare_file_path_list(test_id_list, inp_feat_dir, inp_file_ext)
+out_test_file_list = utils.prepare_file_path_list(test_id_list, out_feat_dir, out_file_ext)
 
-train_x, train_y = read_data_from_list(inp_train_file_list, out_train_file_list, inp_dim, out_dim)
-valid_x, valid_y = read_data_from_list(inp_valid_file_list, out_valid_file_list, inp_dim, out_dim)
+train_x, train_y = utils.read_data_from_list(inp_train_file_list, out_train_file_list, inp_dim, out_dim)
+valid_x, valid_y = utils.read_data_from_list(inp_valid_file_list, out_valid_file_list, inp_dim, out_dim)
